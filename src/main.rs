4446ab390329cf3d499b175deb5b5649795c2bce
Michael Killough <michaeljkillough@gmail.com>
2017-02-11 23:17:37 +0000

    Use hyper's master for tokio goodness.
    Don't actually do anything async yet.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ mod channels;
 extern crate serde_derive;
 extern crate hyper;
 extern crate serde;
+extern crate futures;
 
 use std::io::{Read, Write};
 
-use hyper::version::HttpVersion;
-use hyper::uri::RequestUri;
+use futures::Future;
+use hyper::{HttpVersion, Uri};
 use hyper::status::StatusCode;
-use hyper::server::{Server, Request, Response};
+use hyper::server::{Http, Service, Request, Response};
 use serde::{Deserialize, Serialize};
 use serde::bytes::{ByteBuf, Bytes};
 
@@ -19,26 +20,19 @@ use channels::{ChannelLayer, RedisChannelLayer};
 use asgi::http;
 
 
-fn http_version_to_str(ver: HttpVersion) -> &'static str {
-    match ver {
+fn http_version_to_str(ver: &HttpVersion) -> &'static str {
+    match *ver {
         // ASGI spec doesn't actually allow 0.9 to be used, but it's easiest
         // for us to keep our mouth shut.
         HttpVersion::Http09 => "0.9",
         HttpVersion::Http10 => "1.0",
         HttpVersion::Http11 => "1.1",
-        HttpVersion::Http20 => "2.0",
-    }
-}
-
-fn get_path(uri: &RequestUri) -> &str {
-    match uri {
-        &RequestUri::AbsolutePath(ref path) => path,
-        _ => panic!("Don't know how to handle anything else!"),
+        _ => panic!("Unsupported HTTP version")
     }
 }
 
 fn munge_headers(req: &Request) -> Vec<(ByteBuf, ByteBuf)> {
-    req.headers
+    req.headers()
         .iter()
         .map(|header| {
             // ASGI spec states to lowercase ASCII characters in header names.
@@ -51,48 +45,53 @@ fn munge_headers(req: &Request) -> Vec<(ByteBuf, ByteBuf)> {
         .collect()
 }
 
-fn hello(mut req: Request, mut res: Response) {
-    let channels = RedisChannelLayer::new();
 
-    let mut body = Vec::new();
-    req.read_to_end(&mut body);
+struct AsgiInterface;
 
-    let path = get_path(&req.uri);
+impl Service for AsgiInterface {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item=Response, Error=hyper::Error>>;
 
-    let asgi_req = asgi::http::Request {
-        reply_channel: &channels.new_channel("http.response!"),
-        http_version: &http_version_to_str(req.version).to_owned(),
-        method: req.method.as_ref(),
-        path: path,
-        query_string: "",
-        headers: munge_headers(&req),
-        body: Bytes::new(&body),
-    };
-    channels.send("http.request", &asgi_req);
+    fn call(&self, req: Request) -> Self::Future {
+        let channels = RedisChannelLayer::new();
 
-    // throw away the result, just make sure it does not fail
-    // println!("{:?}", buf);
+        // TODO: Handle body stream and chunking.
+        let body: Vec<u8> = vec![];
+        let asgi_req = asgi::http::Request {
+            reply_channel: &channels.new_channel("http.response!"),
+            http_version: &http_version_to_str(req.version()).to_owned(),
+            method: req.method().as_ref(),
+            path: req.uri().path(),
+            query_string: req.uri().query().unwrap_or(""),
+            headers: munge_headers(&req),
+            body: Bytes::new(&body),
+        };
+        channels.send("http.request", &asgi_req);
 
-    let (channel_name, asgi_resp): (String, asgi::http::Response) = channels.receive(&vec![asgi_req.reply_channel], true).unwrap();
-    println!("{}", channel_name);
+        let (channel, asgi_resp): (String, asgi::http::Response) =
+            channels.receive(&vec![asgi_req.reply_channel], true).unwrap();
 
-    *res.status_mut() = StatusCode::from_u16(asgi_resp.status);
-    for (name, value) in asgi_resp.headers {
-        // TODO: We may set_raw() multiple times for the same header. We should instead
-        // be collecting the duplicate in a values vec and calling set_raw() once.
-        let name = String::from_utf8(name.into()).unwrap();
-        let values = vec![value.into()];
-        res.headers_mut().set_raw(name, values);
+        let mut resp = Response::new();
+        resp.set_status(StatusCode::from_u16(asgi_resp.status));
+        for (name, value) in asgi_resp.headers {
+            // TODO: We may set_raw() multiple times for the same header. We should instead
+            // be collecting the duplicate in a values vec and calling set_raw() once.
+            let name = String::from_utf8(name.into()).unwrap();
+            let value: Vec<u8> = value.into();
+            resp.headers_mut().set_raw(name, value);
+        }
+        let body: Vec<u8> = asgi_resp.content.into();
+        let resp = resp.with_body(body);
+        futures::future::ok(resp).boxed()
     }
-
-    let mut res = res.start().unwrap();
-    res.write_all(&asgi_resp.content);
-    ()
 }
-
 
 fn main() {
     println!("Hello, world!");
 
-    Server::http("0.0.0.0:8080").unwrap().handle(hello).unwrap();
+    let addr = "127.0.0.1:8000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(AsgiInterface)).unwrap();
+    server.run().unwrap();
 }
